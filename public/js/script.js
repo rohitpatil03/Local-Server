b16b39f9786fa99c2b5790d186fefeaddbc99d35
Rohit Patil <rohitpradeeppatil@gmail.com>
2023-01-09 18:19:21 +0530

    Added Create File Feature

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,30 @@
-function onRemove(id){
-    console.log(id)
-    fetch('/delete/' + id,  {
-        method: 'DELETE'
-    })
+function onRemove(id) {
+  console.log(id);
+  fetch("/delete/" + id, {
+    method: "DELETE",
+  });
+}
+
+function createFile() {
+  var inputField = document.getElementsByClassName("popup");
+  var inputValue = inputField[0].value;
+  if (inputValue) {
+    window.location.href = `/edit/${inputValue}`;
+  } else {
+    inputField[0].className = "required popup";
+  }
+}
+
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute(
+    "style",
+    "height:" + tx[i].scrollHeight + "px;overflow-y:hidden;"
+  );
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = this.scrollHeight + "px";
 }
--- server.js
+++ server.js
@@ -1,112 +1,108 @@
-const express = require('express')
-const fileUpload = require('express-fileupload')
-const fs = require('fs')
-const path = require('path')
-const bodyParser = require('body-parser');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const PORT = 8080
-server = express()
+const PORT = 8080;
+server = express();
 
-server.set('view engine', 'ejs')
-server.use(express.static('public'))
-server.use(bodyParser.urlencoded({ extended: true })); 
-server.use(fileUpload())
+server.set("view engine", "ejs");
+server.use(express.static("public"));
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(fileUpload());
 
-let files_array = []
-function checkFilesDir(){
-    const fullPath = path.join(__dirname, 'uploads')
-    fs.readdir(fullPath, (error, files) => {
-        if (error) console.log(error)
-            files_array = files
-        //files.forEach( file => files_array.push(file))
-    })
-
+let files_array = [];
+function checkFilesDir() {
+  const fullPath = path.join(__dirname, "uploads");
+  fs.readdir(fullPath, (error, files) => {
+    if (error) console.log(error);
+    files_array = files;
+    //files.forEach( file => files_array.push(file))
+  });
 }
 
+server.get("/home", (req, res) => {
+  //res.send("Server is running ...")
+  checkFilesDir();
+  res.render("home", { data: files_array });
+});
 
+server.get("/download/:element", (req, res) => {
+  //res.send("Server is running ...")
+  //res.render("home", {"data":files_array})
+  var file = req.params.element;
+  checkFilesDir();
+  res.download(__dirname + `/uploads/${file}`);
+});
 
-server.get("/home", (req, res)=>{
-    //res.send("Server is running ...")
-    checkFilesDir()
-    res.render("home", {"data":files_array})
-})
+server.delete("/delete/:element", (req, res) => {
+  //res.send("Server is running ...")
+  //res.render("home", {"data":files_array})
+  var file = req.params.element;
+  fs.unlink(__dirname + `/uploads/${file}`, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  checkFilesDir();
+  res.render("home", { data: files_array });
+});
 
-server.get("/download/:element", (req, res)=>{
-    //res.send("Server is running ...")
-    //res.render("home", {"data":files_array})
-    var file = req.params.element
-    checkFilesDir()
-    res.download(__dirname+`/uploads/${file}`)
-})
+server.get("/upload", (req, res) => {
+  //res.send("Server is running ...")
+  checkFilesDir();
+  res.render("fileupload", { status: "" });
+});
 
-server.delete("/delete/:element", (req, res)=>{
-    //res.send("Server is running ...")
-    //res.render("home", {"data":files_array})
-    var file = req.params.element
-    fs.unlink(__dirname+`/uploads/${file}`, (err)=>{
-        if(err){
-            console.log(err)
+server.get("/edit/:element", (req, res) => {
+  let file = req.params.element;
+  let data;
+  fs.readFile(__dirname + "/uploads/" + file, "utf8", (err, info) => {
+    if (err) {
+      console.log(err);
+    }
+    data = info;
+    res.render("editfile", { fileName: `${file}`, info: data });
+  });
+});
+
+server.post("/edit/:element", (req, res) => {
+  let file = req.params.element;
+  let data = req.body.fileInfo;
+
+  fs.writeFile(__dirname + "/uploads/" + file, data, (err) => {
+    res.render("editfile", { fileName: `${file}`, info: data });
+  });
+});
+
+server.post("/upload", (req, res) => {
+  if (req.files) {
+    const files = req.files.filename;
+
+    if (files.length == undefined) {
+      files.mv("./uploads/" + files.name, (err) => {
+        if (err) {
+          console.log(err);
         }
-    })
-    checkFilesDir()
-    res.render("home", {"data":files_array})
-})
-
-server.get("/upload", (req, res)=>{
-    //res.send("Server is running ...")
-    checkFilesDir()
-    res.render("fileupload", {"status":""})
-})
-
-server.get("/edit/:element", (req, res)=>{
-    let file = req.params.element
-    let data;
-    fs.readFile(__dirname + '/uploads/' + file, "utf8", (err, info)=>{
-        if (err){
+      });
+      checkFilesDir();
+    } else {
+      for (let i = 0; i < files.length; i++) {
+        files[i].mv("./uploads/" + files[i].name, (err) => {
+          if (err) {
             console.log(err);
-        }
-        data = info
-        res.render("editfile", {"fileName":`${file}`, "info" : data})
-    })
-    
-})
-
-server.post("/edit/:element", (req, res)=>{
-    let file = req.params.element
-    let data = req.body.fileInfo
-
-    fs.writeFile(__dirname + '/uploads/' + file, data, (err) => {
-        res.render("editfile", {"fileName":`${file}`, "info" : data})
-    });
-})
-
-server.post("/upload", (req, res)=>{
-    if (req.files){
-        const files = req.files.filename
-
-        if (files.length==undefined){
-            files.mv('./uploads/'+files.name, (err)=>{
-                if (err){
-                    console.log(err)
-                }
-            })
-            checkFilesDir()
-        }
-        else{
-            for (let i = 0; i < files.length; i++) {
-                files[i].mv('./uploads/'+files[i].name, (err)=>{
-                    if (err){
-                        console.log(err)
-                    }
-                })
-                checkFilesDir()
-            }
-        }
-        res.render("fileupload", {"status":"Uploaded Successfully"})
+          }
+        });
+        checkFilesDir();
+      }
     }
-    else{
-        res.render("fileupload", {"status":"Error in Uploading"})
-    }
-})
+    res.render("fileupload", { status: "Uploaded Successfully" });
+  } else {
+    res.render("fileupload", { status: "Error in Uploading" });
+  }
+});
 
-server.listen(PORT, (req, res)=>{console.log(`Server is running on port ${PORT}`)})
+server.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+});
